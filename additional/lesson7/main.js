ed9cdccff9259ea49756458f1547accc3452aaf5
Roma <kosharko2015@gmail.com>
2022-02-08 13:32:40 +0200

    add additional lesson7

--- additional/lesson7/main.js
+++ additional/lesson7/main.js
@@ -40,8 +40,8 @@
             lat,
             lng,
             companyName,
-            catchPhrase,
-            bs,
+            companyCatchPhrase,
+            companyBs,
         )
         {
             this.id = id;
@@ -60,8 +60,8 @@
             this.address.geo.lng = lng;
             this.company = {};
             this.company.name = companyName;
-            this.company.catchPhrase = catchPhrase;
-            this.company.bs = bs;
+            this.company.catchPhrase = companyCatchPhrase;
+            this.company.bs = companyBs;
         }
     }
 
@@ -83,7 +83,7 @@
         'harness real-time e-markets'
         );
 
-    // console.log(someUser);
+    console.log(someUser);
 
 // -Створити функцію конструктор / клас  який описує обєкт тегу
 //      Поля :
@@ -120,13 +120,59 @@
 //         ]
 //     }
 
-    class HtmlTag {
-        constructor(tagName, tagAction, attributeName, attributeAction) {
-            this.tagName = tagName;
-            this.tagAction = tagAction;
-            this.attr = [];
-            this.attr.push(attributeName)
-            this.attr.push(attributeAction)
+    const htmlTags = [
+        {id: 1, name: '<a>', action: 'створює посилання на іншу сторіку'},
+        {id: 2, name: '<div>', action: 'призначений для виділення фрагмента документа з метою зміни виду вмісту...'},
+        {id: 3, name: '<h1>', action: 'визначає найбільш важливі заголовки'},
+        {id: 4, name: '<span>', action: 'виділяє частину інформації всередині інших тегів та встановл.є для них свій стиль'},
+        {id: 5, name: '<input>', action: 'створює текстові поля, різні кнопки, перемикачі і прапорці...'},
+        {id: 6, name: '<form>', action: 'призначений для обміну даними між користувачем та сервером'},
+        {id: 7, name: '<option>', action: 'призначений для створення пункту списку для тега select чи datalist'},
+        {id: 8, name: '<select>', action: 'створює елемент інтерфейсу у вигляді списку, що розкривається, а також список з одним або множинним вибором'},
+    ];
+    const htmlAttributes = [
+        {tagId: 1, id: 1, name: 'href', action: 'задає адресу документа, на яку слід перейти'},
+        {tagId: 1, id: 2, name: 'type', action: 'вказує MIME-тип документа, на який веде посилання'},
+        {tagId: 2, id: 3, name: 'id', action: 'задає унікальний ідентифікатор для елемента'},
+        {tagId: 2, id: 4, name: 'class', action: 'визначає один або кілька класів, щоб зв’язати елемент з таблицею стилів'},
+        {tagId: 3, id: 5, name: 'title', action: 'вказує додаткову текстову підказку'},
+        {tagId: 3, id: 6, name: 'hidden', action: 'приховує вміст елемента від перегляду'},
+        {tagId: 4, id: 7, name: 'style', action: 'задає вбудований css стиль для елемента'},
+        {tagId: 4, id: 8, name: 'class', action: 'визначає один або кілька класів, щоб зв’язати елемент з таблицею стиліву'},
+        {tagId: 5, id: 9, name: 'type', action: 'повідомляє браузеру, до якого типу належить елемент форми'},
+        {tagId: 5, id: 10, name: 'disabled', action: 'блокує доступ і зміну елемента'},
+        {tagId: 5, id: 11, name: 'value', action: 'задає значення елемента'},
+        {tagId: 6, id: 12, name: 'name', action: 'вказує унікальне імʼя форми'},
+        {tagId: 6, id: 13, name: 'action', action: 'вказує url адресу обробника форми'},
+        {tagId: 7, id: 14, name: 'label', action: 'вказує короткий опис для пункту списку'},
+        {tagId: 7, id: 15, name: 'selected', action: 'задає пункт списку, який буде обраний за дефолтом'},
+        {tagId: 8, id: 16, name: 'required', action: 'обовʼязково для вибору перед відправкою форми'},
+        {tagId: 8, id: 17, name: 'autofocus', action: 'множинний вибір'},
+        {tagId: 8, id: 18, name: 'multiple', action: 'задає пункт списку, який буде обраний за дефолтом'},
+    ];
+
+    class Tag {
+        constructor(id, name, action, attributes) {
+            this.id = id;
+            this.name = name;
+            this.action = action;
+            this.attributes = attributes;
         }
     }
 
+    const createHtmlBook = (tags, attributes) => {
+        const result = [];
+        for (let tag of tags) {
+            let arr = [];
+            for (let attribute of attributes) {
+                if (tag.id === attribute.tagId) {
+                    arr.push(attribute);
+                }
+            }
+            result.push(new Tag(tag.id,tag.name, tag.action, arr));
+        }
+        return result;
+    };
+
+    const htmlBook = createHtmlBook(htmlTags, htmlAttributes);
+    console.log(htmlBook);
