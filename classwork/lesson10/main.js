d369120052ac6e3cb9f4453cdcddabe1d414b348
Roma <kosharko2015@gmail.com>
2022-02-12 11:39:21 +0200

    added fifth point to folder supper lesson9

--- classwork/lesson10/main.js
+++ classwork/lesson10/main.js
@@ -41,6 +41,7 @@
         document.forms.firstForm.input1_OfFirstForm.value = ''
         console.log(document.forms.firstForm.input2_OfFirstForm.value);
         document.forms.firstForm.input2_OfFirstForm.value = '';
+
         console.log(document.forms.secondForm.input1_OfSecondForm.value);
         document.forms.secondForm.input1_OfSecondForm.value = '';
         console.log(document.forms.secondForm.input2_OfSecondForm.value);
@@ -91,7 +92,11 @@
             body.appendChild(table);
         }
         createTable(trNum, tdNum, tdContent);
-    }
+
+        input1.value = '';
+        input2.value = '';
+        input3.value = '';
+    };
 
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
@@ -99,10 +104,73 @@
 //     кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
 
+    const swearWords = ['дурень', 'поц', 'козел', 'телепень', 'мудак', 'падла'];
 
+    let block1 = document.createElement('div');
+    block1.classList.add('block');
+
+    let inputTypeTxt = document.createElement('input');
+    inputTypeTxt.setAttribute('placeholder', 'дайте тексту...');
+
+    let checkWordsBtn = document.createElement('button');
+    checkWordsBtn.innerText = 'провірка';
+
+    block1.append(inputTypeTxt, checkWordsBtn);
+    body.appendChild(block1);
+
+    checkWordsBtn.onclick = function () {
+        let value = inputTypeTxt.value;
+
+        for (let word of swearWords) {
+            if (word === value) {
+                alert('ей ти, не хороший человек.. хорош п.....и !!!');
+                inputTypeTxt.value = '';
+                // break
+                return;
+            }
+        }
+        if (value) {
+            alert('які гарні слова ...');
+            inputTypeTxt.value = '';
+        } else {
+            alert('немає слів..');
+        }
+    };
 
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+
+    let block2 = document.createElement('div');
+    block2.classList.add('block');
+
+    let inputTypeText = document.createElement('input');
+    inputTypeText.setAttribute('placeholder', 'дайте тексту...');
+
+    let checkWordsButton = document.createElement('button');
+    checkWordsButton.innerText = 'провірка';
+
+    block2.append(inputTypeText, checkWordsButton);
+    body.appendChild(block2);
+
+    checkWordsButton.onclick = function () {
+      let value = inputTypeText.value;
+
+      for (let word of swearWords) {
+          if (value.includes(word)) {
+              alert('вибирай слова чоловіче... !!!');
+              inputTypeText.value = '';
+              return;
+          }
+      }
+        if (value) {
+            alert('які гарні слова ...');
+            inputTypeText.value = '';
+        } else {
+            alert('немає слів...');
+        }
+    };
+
+
--- super/lesson9/main.js
+++ super/lesson9/main.js
@@ -1,75 +1,63 @@
 // Є масив :
+// - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
 
     let users = [
-    {
+        {
         name: 'vasya',
         age: 31,
         status: false,
         address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-    },
-    {
+    }, {
         name: 'petya',
         age: 30,
         status: true,
         address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-    },
-    {
+    }, {
         name: 'kolya',
         age: 29,
         status: true,
         address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-    },
-    {
+    }, {
         name: 'olya',
         age: 28,
         status: false,
         address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-    },
-    {
+    }, {
         name: 'max',
         age: 30,
         status: true,
         address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-    },
-    {
+    }, {
         name: 'anya',
         age: 31,
         status: false,
         address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-    },
-    {
+    }, {
         name: 'oleg',
         age: 28,
         status: false,
         address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-    },
-    {
+    }, {
         name: 'andrey',
         age: 29,
         status: true,
         address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-    },
-    {
+    }, {
         name: 'masha',
         age: 30,
         status: true,
         address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-    },
-    {
+    }, {
         name: 'olya',
         age: 31,
         status: false,
         address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-    },
-    {
+    }, {
         name: 'max',
         age: 31,
         status: true,
         address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-    }
-    ];
-
-// - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+    }];
 
     let usersAddress = [];
 
@@ -85,13 +73,12 @@
 //      Всі данні в одному блоці.
 
     let body = document.body;
+    let usersWrap = document.createElement('div');
 
     for (let user of users) {
-        let div = document.createElement('div');
-        div.style.border = '3px solid teal';
-        div.style.padding = '20px';
-        div.style.margin = '30px';
-        div.innerHTML = `name - ${user.name} <br>
+        let userDiv = document.createElement('div');
+        userDiv.classList.add('box');
+        userDiv.innerHTML = `name - ${user.name} <br>
                          age: ${user.age} <br>
                          status - ${user.status} <br>
                          country - ${user.address.country} <br>
@@ -99,47 +86,53 @@
                          street - ${user.address.street} <br>
                          house number: ${user.address.houseNumber}`;
 
-        body.appendChild(div);
+        usersWrap.appendChild(userDiv);
     }
+
+    body.appendChild(usersWrap);
 
 // - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement,
 //      розділивши всі властивості по своїм блокам (div>div*4)
 
+    let userCards = document.createElement('div');
+
     for (let user of users) {
-        let userCard = document.createElement('div');
-        userCard.style.border = '3px solid silver';
-        userCard.style.padding = '20px';
-        userCard.style.margin = '30px';
+            let userCard = document.createElement('div');
+            userCard.classList.add('box');
+            userCard.style.borderColor = '#e2a932';
 
-        let username = document.createElement('div');
-        username.innerText = `name - ${user.name}`;
+            let username = document.createElement('div');
+            username.innerText = `name - ${user.name}`;
 
-        let age = document.createElement('div');
-        age.innerText = `age: ${user.age}`;
+            let age = document.createElement('div');
+            age.innerText = `age: ${user.age}`;
 
-        let status = document.createElement('div');
-        status.innerText = `status - ${user.status}`;
+            let status = document.createElement('div');
+            status.innerText = `status - ${user.status}`;
 
-        let address = document.createElement('div');
-        address.innerHTML = `country - ${user.address.country} <br>
-                             city - ${user.address.city} <br>
-                             street - ${user.address.street} <br>
-                             house number: ${user.address.houseNumber}`;
+            let address = document.createElement('div');
+            address.innerHTML = `country - ${user.address.country} <br>
+                                 city - ${user.address.city} <br>
+                                 street - ${user.address.street} <br>
+                                 house number: ${user.address.houseNumber}`;
 
-        userCard.append(username, age, status, address);
-        body.appendChild(userCard);
+            userCard.append(username, age, status, address);
+            userCards.appendChild(userCard);
     }
+
+    body.appendChild(userCards);
 
 // - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement,
 //      розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
 
-    for (let user of users) {
-        let userCard = document.createElement('div');
-        userCard.style.border = '3px solid green';
-        userCard.style.padding = '20px';
-        userCard.style.margin = '30px';
+    let usersDiv = document.createElement('div');
 
-        let username = document.createElement('h2');
+    for (let user of users) {
+        let userDiv = document.createElement('div');
+        userDiv.classList.add('box');
+        userDiv.style.borderColor = '#9d1ca7';
+
+        let username = document.createElement('h3');
         username.innerText = `name:  ${user.name}`;
 
         let userAge = document.createElement('div');
@@ -149,6 +142,7 @@
         userStatus.innerText = `status: ${user.status}`;
 
         let userAddress = document.createElement('div');
+
         let country = document.createElement('div');
         country.innerText = `country: ${user.address.country}`;
 
@@ -162,62 +156,77 @@
         houseNumber.innerText = `house number: ${user.address.houseNumber}`;
 
         userAddress.append(country, city, street, houseNumber);
-        userCard.append(username, userAge, userStatus, userAddress);
-        body.append(userCard);
+
+        userDiv.append(username, userAge, userStatus, userAddress);
+        usersDiv.appendChild(userDiv);
     }
 
-// - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+    body.appendChild(usersDiv);
+
+// - є сторінка rules.html. Контентом сторінки є заголовки та параграфи.
+//     Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
 //     створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того,
 //     що знаходиться на сторінці.
 //     Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
 
-//    todo Recursion function
+    let divIdContent = document.getElementById('content');
+
+    let headings = document.getElementsByTagName('h2');
+    let titlesList = document.createElement('ul');
+
+    for (let heading of headings) {
+        let li = document.createElement('li');
+        li.innerText = heading.innerText;
+        titlesList.appendChild(li);
+    }
+
+    divIdContent.appendChild(titlesList);
 
 // -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить кожне правило в окремому блоці.
 //     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 //     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
 
     let rules = [
-    {
-        title: 'Первое правило Бойцовского клуба.',
-        body: 'Никому не рассказывать о Бойцовском клубе.'
-    },
-    {
-        title: 'Второе правило Бойцовского клуба.',
-        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-    },
-    {
-        title: 'Третье правило Бойцовского клуба.',
-        body: 'В схватке участвуют только двое.'
-    },
-    {
-        title: 'Четвертое правило Бойцовского клуба.',
-        body: 'Не более одного поединка за один раз.'
-    },
-    {
-        title: 'Пятое правило Бойцовского клуба.',
-        body: 'Бойцы сражаются без обуви и голые по пояс.'
-    },
-    {
-        title: 'Шестое правило Бойцовского клуба.',
-        body: 'Поединок продолжается столько, сколько потребуется.'
-    },
-    {
-        title: 'Седьмое правило Бойцовского клуба.',
-        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-    },
-    {
-        title: 'Восьмое и последнее правило Бойцовского клуба.',
-        body: 'Новичок обязан принять бой.'
-    },
+        {
+            title: 'Первое правило Бойцовского клуба.',
+            body: 'Никому не рассказывать о Бойцовском клубе.'
+        },
+        {
+            title: 'Второе правило Бойцовского клуба.',
+            body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+        },
+        {
+            title: 'Третье правило Бойцовского клуба.',
+            body: 'В схватке участвуют только двое.'
+        },
+        {
+            title: 'Четвертое правило Бойцовского клуба.',
+            body: 'Не более одного поединка за один раз.'
+        },
+        {
+            title: 'Пятое правило Бойцовского клуба.',
+            body: 'Бойцы сражаются без обуви и голые по пояс.'
+        },
+        {
+            title: 'Шестое правило Бойцовского клуба.',
+            body: 'Поединок продолжается столько, сколько потребуется.'
+        },
+        {
+            title: 'Седьмое правило Бойцовского клуба.',
+            body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+        },
+        {
+            title: 'Восьмое и последнее правило Бойцовского клуба.',
+            body: 'Новичок обязан принять бой.'
+        },
+    ];
 
-];
+    let wrap = document.createElement('div');
 
     for (let rule of rules) {
-        let block = document.createElement('div');
-        block.style.border = '3px solid #000';
-        block.style.padding = '20px';
-        block.style.margin = '20px';
+        let ruleDiv = document.createElement('div');
+        ruleDiv.classList.add('box');
+        ruleDiv.style.borderColor = 'tomato';
 
         let title = document.createElement('h2');
         title.innerText = rule.title;
@@ -225,8 +234,8 @@
         let text = document.createElement('p');
         text.innerText = rule.body;
 
-        block.append(title, text);
-        body.appendChild(block);
+        ruleDiv.append(title, text);
+        wrap.appendChild(ruleDiv);
     }
 
-
+    body.appendChild(wrap);
